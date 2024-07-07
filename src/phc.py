8081cc4d8a538b6ae5bc818db04e3f9ac7b07a34
Victor DELAUZUN DESCHAMPS <victor.delauzundeschamps@amadeus.com>
2024-07-07 12:09:15 +0200

    --noconsole with quotes

--- src/phc.py
+++ src/phc.py
@@ -1,8 +1,14 @@
-import pystray, subprocess, sys, os
-import psutil, time, winshell
+# pylint: disable = line-too-long, unused-argument, subprocess-run-check
+# pylint: disable = missing-module-docstring, import-error, no-name-in-module, ungrouped-imports
+import sys
+import os
+from time import sleep
+from subprocess import run, PIPE
+from psutil import win_service_get
 from win32com.client import Dispatch
 from PIL import Image, ImageDraw
-import win32com.shell.shell as shell
+from win32com.shell.shell import ShellExecuteEx
+from pystray import Icon, Menu, MenuItem
 
 # To run:
 # python -m pip install -r .\src\requirements.txt
@@ -46,7 +52,7 @@ def get_service(name):
     """Get the specified service as psutil.service object."""
     service = None
     try:
-        service = psutil.win_service_get(name)
+        service = win_service_get(name)
     except Exception as ex:
         # raise psutil.NoSuchProcess if no service with such name exists
         print(str(ex))
@@ -59,7 +65,7 @@ def is_service_running(name):
 
 def has_AU_rights_for(name):
     """Check if right has been granted to AU (AuthenticatedUsers) to start and stop the specified service."""
-    process = subprocess.run(["sc", 'sdshow', name], stdout=subprocess.PIPE, text=True)
+    process = run(["sc", 'sdshow', name], stdout=PIPE, text=True)
     if MAGIC_SDDL in process.stdout.rstrip():
         return True
     return False
@@ -67,18 +73,18 @@ def has_AU_rights_for(name):
 def service_set_status(name, up):
     """Start or stop specified service."""
     if not has_AU_rights_for(name):
-        setRights(True)
+        set_rights(True)
     if up and get_service_starttype(name) == "Disabled":
         set_service_starttype(name,'demand')
-    process = subprocess.run(["net", ("stop","start")[up], name], stdout=subprocess.PIPE, text=True)
+    run(["net", ("stop","start")[up], name], stdout=PIPE, text=True)
 
 def reflesh_status():
     """Refresh all status in case something had been changed by somthing else than this software."""
     global states
-    global mysystrayicon
+    global my_systray_icon
     states = {'stAgentSvc': is_service_running('stAgentSvc'), 'pangps': is_service_running('pangps'), 'webclient': is_service_running('webclient'), 'startup': is_run_at_startup_enabled()}
-    if mysystrayicon:
-        mysystrayicon.icon=create_image(64, 64, 'red', 'blue',[states['stAgentSvc'],states['pangps'],not states['webclient']])
+    if my_systray_icon:
+        my_systray_icon.icon=create_image(64, 64, 'red', 'blue',[states['stAgentSvc'],states['pangps'],not states['webclient']])
 
 def on_service_clicked(icon, item, service):
     """Toggle service status"""
@@ -96,21 +102,21 @@ def create_image(width, height, color1, color2, list_of_band):
                 fill=color2)
     return image
 
-def setRights(give):
-    AURights = ("",MAGIC_SDDL)[give]
+def set_rights(give):
+    AU_rights = ("",MAGIC_SDDL)[give]
     webclient_starttype = ("disabled","demand")[give]
-    shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c '+'sc.exe sdset stAgentSvc "D:(A;;CCLCSWRPLORCWD;;;BA)'+AURights+
-                         '(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)S:(AU;FA;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;WD)" && sc.exe sdset pangps "D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)'+AURights+
-                         '(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)S:(AU;FA;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;WD)" && sc.exe sdset webclient "D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)'+AURights+
+    ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters='/c '+'sc.exe sdset stAgentSvc "D:(A;;CCLCSWRPLORCWD;;;BA)'+AU_rights+
+                         '(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)S:(AU;FA;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;WD)" && sc.exe sdset pangps "D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)'+AU_rights+
+                         '(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)S:(AU;FA;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;WD)" && sc.exe sdset webclient "D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)'+AU_rights+
                          '(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)" && sc.exe config webclient start= '+webclient_starttype)
-    time.sleep(1)
+    sleep(1)
 
 def get_service_starttype(service):
-    process = subprocess.run(["powershell", f"Get-Service -Name {service} | select -ExpandProperty starttype"], stdout=subprocess.PIPE, text=True)
+    process = run(["powershell", f"Get-Service -Name {service} | select -ExpandProperty starttype"], stdout=PIPE, text=True)
     return process.stdout.strip()
 
 def set_service_starttype(service, starttype):
-    shell.ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters=f'/c sc.exe config {service} start= '+starttype)
+    ShellExecuteEx(lpVerb='runas', lpFile='cmd.exe', lpParameters=f'/c sc.exe config {service} start= '+starttype)
 
 def be_free(icon,item):
     service_set_status('pangps',False)
@@ -130,13 +136,13 @@ def victor_the_cleaner(icon,item):
     service_set_status('webclient',False)
     service_set_status('stAgentSvc',True)
     service_set_status('pangps',True)
-    setRights(False)
+    set_rights(False)
     run_at_startup(False)
     icon.notify('Ass clean.')
     terminate(icon,item)
 
 def abort_shutdown():
-    process = subprocess.run(['shutdown','-a'], stdout = subprocess.PIPE)
+    run(['shutdown','-a'], stdout = PIPE)
 
 def terminate(icon, item):
     icon.stop()
@@ -144,48 +150,48 @@ def terminate(icon, item):
 
 ####
 
-mysystrayicon=None
+my_systray_icon=None
 reflesh_status()
-mymenu = pystray.Menu(
-        pystray.MenuItem(
+my_menu = Menu(
+        MenuItem(
             'Be free !!',
             be_free),
-        pystray.MenuItem(
+        MenuItem(
             'Be corporate !!',
             be_corporate),
-        pystray.Menu.SEPARATOR,
-        pystray.MenuItem(
+        Menu.SEPARATOR,
+        MenuItem(
             'Netscope',
             lambda icon, item: on_service_clicked(icon, item, 'stAgentSvc'),
             checked=lambda item: states['stAgentSvc']),
-        pystray.MenuItem(
+        MenuItem(
             'Global Protect',
             lambda icon, item: on_service_clicked(icon, item, 'pangps'),
             checked=lambda item: states['pangps']),
-        pystray.MenuItem(
+        MenuItem(
             'Webclient',
             lambda icon, item: on_service_clicked(icon, item, 'webclient'),
             checked=lambda item: states['webclient']),
-        pystray.Menu.SEPARATOR,
-        pystray.MenuItem(
+        Menu.SEPARATOR,
+        MenuItem(
             'Run at startup',
             lambda icon, item: run_at_startup(not item.checked),
             checked=lambda item: states['startup']),
-        pystray.Menu.SEPARATOR,
-        pystray.MenuItem(
+        Menu.SEPARATOR,
+        MenuItem(
             'Abort planned Shutdown',
             lambda icon, item: abort_shutdown()),
-        pystray.Menu.SEPARATOR,
-        pystray.MenuItem(
+        Menu.SEPARATOR,
+        MenuItem(
             'Erase the evidences && Exit',
             victor_the_cleaner),
-        pystray.Menu.SEPARATOR,
-        pystray.MenuItem(
+        Menu.SEPARATOR,
+        MenuItem(
             'Exit',
             terminate))
-mysystrayicon = pystray.Icon(
+my_systray_icon = Icon(
     'test name',
     icon=None,
-    menu=mymenu)
+    menu=my_menu)
 reflesh_status()
-mysystrayicon.run()
+my_systray_icon.run()
